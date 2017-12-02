68a3ebac88d05764d3e18907af968c03d83bedf8
Nicolai Birkmose Oksen <nicolaioksen@gmail.com>
2017-12-02 12:57:06 +0100

    Add tests to repo

---
+++ AdventOfCodeTests/Day1Tests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdventOfCode;
+using Xunit;
+
+namespace AdventOfCodeTests
+{
+    public class Day1Tests
+    {
+        // 1122 produces a sum of 3 (1 + 2) because the first digit (1) matches the second digit and the third digit (2) matches the fourth digit.
+        [Fact]
+        public void OneOneTwoTwoSumOfThreeTest()
+        {
+            var day1 = new Day1("1122");
+
+            var result = day1.GetCaptchaSolution();
+
+            Assert.Equal("3", result);
+        }
+
+        // 1111 produces 4 because each digit(all 1) matches the next.
+        [Fact]
+        public void OneOneOneOneSumOfFourTest()
+        {
+            var day1 = new Day1("1111");
+
+            var result = day1.GetCaptchaSolution();
+
+            Assert.Equal("4", result);
+        }
+
+        // 1234 produces 0 because no digit matches the next.
+        [Fact]
+        public void OneTwoThreeFourSumOfZeroTest()
+        {
+            var day1 = new Day1("1234");
+
+            var result = day1.GetCaptchaSolution();
+
+            Assert.Equal("0", result);
+        }
+
+        // 91212129 produces 9 because the only digit that matches the next one is the last digit, 9.
+        [Fact]
+        public void _91212129SumOfNineTest()
+        {
+            var day1 = new Day1("91212129");
+
+            var result = day1.GetCaptchaSolution();
+
+            Assert.Equal("9", result);
+        }
+    }
+}
---
+++ AdventOfCodeTests/Day2Tests.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using AdventOfCode;
+using Xunit;
+
+namespace AdventOfCodeTests
+{
+    public class Day2Tests
+    {
+        /*
+         * 5 1 9 5
+         * 7 5 3
+         * 2 4 6 8
+         * The first row's largest and smallest values are 9 and 1, and their difference is 8.
+         * The second row's largest and smallest values are 7 and 3, and their difference is 4.
+         * The third row's difference is 6.
+         * In this example, the spreadsheet's checksum would be 8 + 4 + 6 = 18.
+         */
+
+        private IList<int[]> Table => new List<int[]>
+        {
+            new int[] {5, 1, 9, 5},
+            new int[] {7, 5, 3},
+            new int[] {2, 4, 6, 8}
+        };
+
+        [Fact]
+        public void FirstRowsDifferenceIsEight()
+        {
+            var day2 = new Day2(Table);
+
+            var result = day2.GetDifferenceForRow(0);
+
+            Assert.Equal(8, result);
+        }
+
+        [Fact]
+        public void SecondRowDifferenceIsFour()
+        {
+            var day2 = new Day2(Table);
+
+            var result = day2.GetDifferenceForRow(1);
+
+            Assert.Equal(4, result);
+        }
+
+        [Fact]
+        public void ThirdRowDifferenceIsSix()
+        {
+            var day2 = new Day2(Table);
+
+            var result = day2.GetDifferenceForRow(2);
+
+            Assert.Equal(6, result);
+        }
+
+        [Fact]
+        public void ChecksumIs18()
+        {
+            var day2 = new Day2(Table);
+
+            var result = day2.CalculateChecksumOfTable();
+
+            Assert.Equal(18, result);
+        }
+    }
+}
