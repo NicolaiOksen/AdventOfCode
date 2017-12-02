c4aa11334f5d65dc32310622865348a7a555adbd
Nicolai Birkmose Oksen <nicolaioksen@gmail.com>
2017-12-02 12:54:36 +0100

    Add solution for day 2

---
+++ AdventOfCode/Day2.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode
+{
+    public class Day2
+    {
+        public IList<int[]> Table { get; }
+
+        public Day2(IList<int[]> table)
+        {
+            Table = table;
+        }
+
+        public int CalculateChecksumOfTable()
+        {
+            return Table.Sum(GetDifferenceForRow);
+        }
+
+        private int GetDifferenceForRow(int[] row)
+        {
+            return row.Max() - row.Min();
+        }
+
+        public int GetDifferenceForRow(int rowIndex)
+        {
+            return GetDifferenceForRow(Table[rowIndex]);
+        }
+
+        public override string ToString()
+        {
+            return $"Day 2 of Christmas: {CalculateChecksumOfTable()}";
+        }
+    }
+}
